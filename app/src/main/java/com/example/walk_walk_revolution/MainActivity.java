e80e4bb897e93d183a9efe0d8f4c0cb12e906f04
LourenArjon <loarjon@ucsd.edu>
2020-02-06 15:32:05 -0800

    initial commit for step counter

--- app/src/main/java/com/example/walk_walk_revolution/MainActivity.java
+++ app/src/main/java/com/example/walk_walk_revolution/MainActivity.java
@@ -2,13 +2,94 @@ package com.example.walk_walk_revolution;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private String fitnessServiceKey = "GOOGLE_FIT";
+    public static final String TAG = "MainActivity";
+
+    private TextView textSteps;
+    private FitnessService fitnessService;
+    private UpdateCounter runner;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textSteps = findViewById(R.id.CurrentSteps);
+        UpdateCounter runner = new UpdateCounter();
+
+        FitnessServiceFactory.put(fitnessServiceKey, new FitnessServiceFactory.BluePrint() {
+            @Override
+            public FitnessService create(MainActivity activity) {
+                return new GoogleFitAdapter(activity);
+            }
+        });
+
+        fitnessService = FitnessServiceFactory.create(fitnessServiceKey, this);
+        fitnessService.setup();
+
+        runner.execute();
+
+
+
     }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        fitnessService.updateStepCount();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+//       If authentication was required during google fit setup, this will be called after the user authenticates
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == fitnessService.getRequestCode()) {
+                fitnessService.updateStepCount();
+            }
+        } else {
+            Log.e(TAG, "ERROR, google fit result code: " + resultCode);
+        }
+    }
+
+    public void setStepCount(long stepCount) {
+        textSteps.setText(String.valueOf(stepCount));
+    }
+
+    private class UpdateCounter extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            while(!isCancelled())
+            {
+                try{
+                    publishProgress();
+                    Thread.sleep(1000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(String... text)
+        {
+            fitnessService.updateStepCount();
+        }
+
+    }
+
 }
