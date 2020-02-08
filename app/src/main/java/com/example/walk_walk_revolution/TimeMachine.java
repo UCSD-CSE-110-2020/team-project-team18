b837a07cbac08f027451d7be138b09f7c12d93ec
Ryan <r6jackso@ucsd.edu>
2020-02-07 20:39:51 -0800

    set to 16

--- app/src/main/java/com/example/walk_walk_revolution/TimeMachine.java
+++ app/src/main/java/com/example/walk_walk_revolution/TimeMachine.java
@@ -5,23 +5,23 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 
 public class TimeMachine {
-    private static Clock clock = Clock.systemDefaultZone();
-    private static ZoneId zoneId = ZoneId.systemDefault();
+  //  private static Clock clock = Clock.systemDefaultZone();
+  //  private static ZoneId zoneId = ZoneId.systemDefault();
 
-    public static LocalDateTime now() {
-        return LocalDateTime.now(getClock());
-    }
+   // public static LocalDateTime now() {
+  //      return LocalDateTime.now(getClock());
+   // }
 
     public static void useFixedClockAt(LocalDateTime date){
-        clock = Clock.fixed(date.atZone(zoneId).toInstant(), zoneId);
+     //   clock = Clock.fixed(date.atZone(zoneId).toInstant(), zoneId);
     }
 
-    public static void useSystemDefaultZoneClock(){
-        clock = Clock.systemDefaultZone();
+   public static void useSystemDefaultZoneClock(){
+      //  clock = Clock.systemDefaultZone();
     }
 
-    private static Clock getClock() {
-        return clock ;
-    }
+   // private static Clock getClock() {
+    //    return clock ;
+ //   }
 
 }
