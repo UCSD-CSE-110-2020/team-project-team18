1fe90a9b188a599797c4331053156cd7ab4f3789
yootoohzi <yootoohzi@gmail.com>
2020-03-07 13:15:18 -0800

    updated scheduled walk

---
+++ app/src/main/java/com/example/walk_walk_revolution/ScheduledWalkScreen.java
@@ -0,0 +1,15 @@
+package com.example.walk_walk_revolution;
+
+import android.app.AppComponentFactory;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ScheduledWalkScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scheduled_walk_screen);
+    }
+}
