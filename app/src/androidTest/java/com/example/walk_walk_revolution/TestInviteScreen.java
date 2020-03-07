520f546885fd4325ddbfca06bd1547877647076d
LourenArjon <loarjon@ucsd.edu>
2020-03-07 13:12:14 -0800

    writing tests

---
+++ app/src/androidTest/java/com/example/walk_walk_revolution/TestInviteScreen.java
@@ -0,0 +1,12 @@
+package com.example.walk_walk_revolution;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.runner.RunWith;
+
+//@RunWith(AndroidJUnit4)
+public class TestInviteScreen {
+    private static final String TEST_SERVICE = "TEST_SERVICE";
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+}
